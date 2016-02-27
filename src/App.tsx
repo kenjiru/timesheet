c33f11e35ec1c885b305c74954a37c59f9ba9f95
kenjiru <kenjiru.ro@gmail.com>
2016-02-28 00:57:24 +0100

    Use the react-bootstrap components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,43 @@
 import 'babel-polyfill';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Grid, Row, Col } from "react-bootstrap";
+import AddTask from "./components/add-task/AddTask";
+
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
 
 import './App.less';
 
-class App extends React.Component<any, any> {
+class App extends React.Component<IAppProps, IAppState> {
+    constructor(props:IAppProps) {
+        super(props);
+    }
+
     render() {
         return (
-            <div>
-                <h1>App title</h1>
-                <div>App content</div>
+            <div className="app">
+                {this.renderHeader()}
+                <AddTask/>
             </div>
+        )
+    }
+
+    renderHeader() {
+        return (
+            <Grid className="app" fluid={true}>
+                <Row>
+                    <Col md={12}>
+                        <h1>Timesheet</h1>
+                    </Col>
+                </Row>
+            </Grid>
         );
     }
 }
+
+interface IAppProps {}
+
+interface IAppState {}
 
 ReactDOM.render(<App/>, document.body);
